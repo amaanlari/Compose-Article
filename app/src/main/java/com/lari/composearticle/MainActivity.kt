7b7804f04c620846fed51348a2c22e0f3d9b4de5
amaanlari <amaanlari@outlook.com>
2023-08-20 17:29:06 +0530

    Add banner image of Article

--- app/src/main/java/com/lari/composearticle/MainActivity.kt
+++ app/src/main/java/com/lari/composearticle/MainActivity.kt
@@ -3,11 +3,16 @@ package com.lari.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.lari.composearticle.ui.theme.ComposeArticleTheme
 
@@ -21,18 +26,29 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    ArticleImage()
                 }
             }
         }
     }
 }
 
+@Composable
+fun ArticleImage() {
+    val composeImage = painterResource(id = R.drawable.bg_compose_background)
+    Image(
+        painter = composeImage,
+        contentDescription = null,
+        modifier = Modifier.fillMaxWidth(),
+        alignment = Alignment.TopCenter,
+        contentScale = ContentScale.Fit
+    )
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeArticleTheme {
-
+        ArticleImage()
     }
 }
