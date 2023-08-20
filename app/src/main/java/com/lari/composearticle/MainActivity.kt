7ce97d9a31c261853329978af1d9019b9b2d0cf1
amaanlari <amaanlari@outlook.com>
2023-08-20 18:06:01 +0530

    Add compose article texts in Column

--- app/src/main/java/com/lari/composearticle/MainActivity.kt
+++ app/src/main/java/com/lari/composearticle/MainActivity.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.lari.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,12 +33,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ArticleImage()
+                    ArticleText()
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun ArticleImage() {
@@ -45,10 +53,31 @@ fun ArticleImage() {
     )
 }
 
+@Composable
+fun ArticleText() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = stringResource(id = R.string.title_text),
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp,
+        )
+        Text(
+            text = stringResource(id = R.string.intro_para),
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = stringResource(id = R.string.tutorial_para),
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeArticleTheme {
         ArticleImage()
+        ArticleText()
     }
 }
